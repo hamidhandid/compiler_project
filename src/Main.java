92ed840c3a4ba1e28a19c88b019542e36b50cd2a
Hamid <montazeria90@gmail.com>
2021-01-21 13:50:53 +0330

    add Main.java that is the main file for compiler

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import codegen.CodeGenerator;
+import parser.Parser;
+import scanner.classes.CompilerScanner;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            CompilerScanner scanner = new CompilerScanner(new FileReader("src/scanner/files/code1.txt"));
+            CodeGenerator codeGen = new CodeGenerator();
+            Parser parser = new Parser(scanner, codeGen, "src/parser/table.npt", true);
+            parser.parse();
+        } catch (FileNotFoundException e) {
+            System.err.println("Can not read file");
+            e.printStackTrace();
+        }
+    }
+}
