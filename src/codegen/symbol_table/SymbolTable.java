678e4ba29b0e7ffa3cf9cda9e0cb0dea0da7026a
Hamid <montazeria90@gmail.com>
2021-01-25 15:36:29 +0330

    Add SymbolTable

--- src/codegen/symbol_table/SymbolTable.java
+++ src/codegen/symbol_table/SymbolTable.java
@@ -1,4 +1,30 @@
 package codegen.symbol_table;
 
+import codegen.symbol_table.dscp.Descriptor;
+
+import java.util.Map;
+
 public class SymbolTable {
+    private Map<String, Descriptor> symbolTable;
+
+    public SymbolTable(Map<String, Descriptor> symbolTable) {
+        this.symbolTable = symbolTable;
+    }
+
+    public boolean contains(Descriptor descriptor) {
+        return symbolTable.containsKey(descriptor.getName());
+    }
+
+    public void addDescriptor(Descriptor descriptor) {
+        symbolTable.put(descriptor.getName(), descriptor);
+    }
+
+    public Descriptor getDescriptor(String name) {
+        return symbolTable.get(name);
+    }
+
+
+    public Map<String, Descriptor> getSymbolTable() {
+        return symbolTable;
+    }
 }
