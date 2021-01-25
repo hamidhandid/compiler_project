a81c40515a0331b7063d1c6b3abb36fe679f5a42
Hamid <montazeria90@gmail.com>
2021-01-25 15:33:23 +0330

    Add VariableDescriptor

---
+++ src/codegen/symbol_table/dscp/variables/VariableDescriptor.java
@@ -0,0 +1,10 @@
+package codegen.symbol_table.dscp.variables;
+
+import codegen.symbol_table.dscp.Descriptor;
+import scanner.classes.Type;
+
+public class VariableDescriptor extends Descriptor {
+    public VariableDescriptor(String name, Type type, boolean isLocal) {
+        super(name, type, isLocal);
+    }
+}
