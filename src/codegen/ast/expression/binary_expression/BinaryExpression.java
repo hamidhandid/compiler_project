3f68e8d033a5245941d70b20c24d8bed5a010d92
Hamid <montazeria90@gmail.com>
2021-01-28 13:08:06 +0330

    Fix BinaryExpression code

--- src/codegen/ast/expression/binary_expression/BinaryExpression.java
+++ src/codegen/ast/expression/binary_expression/BinaryExpression.java
@@ -1,19 +1,22 @@
 package codegen.ast.expression.binary_expression;
 
+import codegen.CodeGenerator;
 import codegen.ast.expression.Expression;
-import codegen.ast.expression.constant.IntegerConstant;
-import codegen.symbol_table.SymbolTable;
 import codegen.symbol_table.dscp.Descriptor;
-import codegen.symbol_table.stacks.SymbolTableStack;
+import codegen.symbol_table.dscp.variables.LocalVariableDescriptor;
+import codegen.symbol_table.stacks.SemanticStack;
+import codegen.utils.AssemblyFileWriter;
+import codegen.utils.DescriptorChecker;
 import codegen.utils.type.TypeChecker;
+import scanner.classes.Type;
 
 public abstract class BinaryExpression extends Expression {
 
-    protected Expression firstOperand;
-    protected Expression secondOperand;
+    protected Object firstOperand;
+    protected Object secondOperand;
     protected String operation;
 
-    public BinaryExpression(Expression firstOperand, Expression secondOperand, String operation) {
+    public BinaryExpression(Object firstOperand, Object secondOperand, String operation) {
         this.firstOperand = firstOperand;
         this.secondOperand = secondOperand;
         this.operation = operation;
@@ -21,24 +24,34 @@ public abstract class BinaryExpression extends Expression {
 
     @Override
     public void compile() {
-//        if (firstOperand instanceof Descriptor && secondOperand instanceof Descriptor) {
-//            Descriptor firstOperandDes = (Descriptor) firstOperand;
-//            Descriptor secondOperandDes = (Descriptor) secondOperand;
-//            SymbolTable st = SymbolTableStack.top();
-//            if (st.containsDescriptor(firstOperandDes.getName()) && st.containsDescriptor(secondOperandDes.getName())) {
-//                TypeChecker.checkType(firstOperandDes.getType(), secondOperandDes.getType(), operation);
-//            } else {
-//                System.err.println("Not Defined Before");
-//            }
-//        } else if (firstOperand instanceof IntegerConstant && secondOperand instanceof IntegerConstant) {
-//            IntegerConstant firstOperandIntConst = (IntegerConstant) firstOperand;
-//            IntegerConstant secondOperandIntConst = (IntegerConstant) secondOperand;
-//            TypeChecker.checkType(firstOperandIntConst.getType(), secondOperandIntConst.getType(), operation);
-//        }
-        // SemanticStack.getInstance().push(firstOperand + secondOperand);
-        // set opcode
-        // set number1
-        // set number2
-        // generate code of BinaryExpression with opcode
+        System.out.println("BinaryExpr");
+        Descriptor firstOperandDes = (Descriptor) firstOperand;
+        Descriptor secondOperandDes = (Descriptor) secondOperand;
+        TypeChecker.checkType(firstOperandDes.getType(), secondOperandDes.getType(), "add");
+        DescriptorChecker.checkContainsDescriptor(firstOperandDes);
+        DescriptorChecker.checkContainsDescriptor(secondOperandDes);
+
+        String operationCommand;
+        Type type;
+        switch (operation) {
+            case "+":
+                operationCommand = "add";
+                type = Type.INTEGER_NUMBER;
+                break;
+            default:
+                operationCommand = null;
+                type = null;
+        }
+        String variableName = CodeGenerator.getVariableName();
+        AssemblyFileWriter.appendComment("binary " + operationCommand + " expression of " + firstOperandDes.getName() + ", " + secondOperandDes.getName() );
+        AssemblyFileWriter.appendCommandToCode("la", "$t0", firstOperandDes.getName());
+        AssemblyFileWriter.appendCommandToCode("la", "$t1", secondOperandDes.getName());
+        AssemblyFileWriter.appendCommandToCode("lw", "$t0", "0($t0)");
+        AssemblyFileWriter.appendCommandToCode("lw", "$t1", "0($t1)");
+        AssemblyFileWriter.appendCommandToCode(operationCommand, "$t0", "$t0", "$t1");
+        AssemblyFileWriter.appendCommandToData(variableName, "word", "0");
+        AssemblyFileWriter.appendCommandToCode("sw", "$t0", variableName);
+        AssemblyFileWriter.appendDebugLine(variableName);
+        SemanticStack.push(new LocalVariableDescriptor(variableName, type));
     }
 }
--- src/codegen/ast/expression/binary_expression/arithmetic/Add.java
+++ src/codegen/ast/expression/binary_expression/arithmetic/Add.java
@@ -1,26 +1,14 @@
 package codegen.ast.expression.binary_expression.arithmetic;
 
-import codegen.ast.expression.Expression;
 import codegen.ast.expression.binary_expression.BinaryExpression;
-import codegen.ast.expression.constant.IntegerConstant;
-import codegen.symbol_table.dscp.Descriptor;
-import codegen.utils.AssemblyFileWriter;
 
 public class Add extends BinaryExpression {
-    public Add(Expression firstOperand, Expression secondOperand) {
+    public Add(Object firstOperand, Object secondOperand) {
         super(firstOperand, secondOperand, "+");
     }
 
     @Override
     public void compile() {
         super.compile();
-//        if (firstOperand instanceof Descriptor && secondOperand instanceof Descriptor) {
-//            Descriptor secondDes = (Descriptor) secondOperand;
-//            Descriptor firstDes = (Descriptor) firstOperand;
-//        } else if (firstOperand instanceof IntegerConstant && secondOperand instanceof IntegerConstant) {
-//            IntegerConstant firstIntConst = (IntegerConstant) firstOperand;
-//            IntegerConstant secondIntConst = (IntegerConstant) secondOperand;
-//            AssemblyFileWriter.appendCommandToCode("la", "");
-//        }
     }
 }
