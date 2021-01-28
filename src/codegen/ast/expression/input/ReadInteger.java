6a9e1ac423263d7be437c34c7c69da9ba6cd0112
Hamid <montazeria90@gmail.com>
2021-01-28 13:15:08 +0330

    Fix ReadInteger code

--- src/codegen/ast/expression/input/ReadInteger.java
+++ src/codegen/ast/expression/input/ReadInteger.java
@@ -1,14 +1,24 @@
 package codegen.ast.expression.input;
 
+import codegen.CodeGenerator;
 import codegen.ast.expression.Expression;
+import codegen.symbol_table.dscp.variables.LocalVariableDescriptor;
+import codegen.symbol_table.stacks.SemanticStack;
 import codegen.utils.AssemblyFileWriter;
+import scanner.classes.Type;
 
 public class ReadInteger extends Expression {
     @Override
     public void compile() {
-        AssemblyFileWriter.appendCommandToCode("# read integer");
+        AssemblyFileWriter.appendComment("read integer");
         AssemblyFileWriter.appendCommandToCode("li", "$v0", "5");
         AssemblyFileWriter.appendCommandToCode("syscall");
         AssemblyFileWriter.appendCommandToCode("move", "$t0", "$v0");
+        String variableName = CodeGenerator.getVariableName();
+        AssemblyFileWriter.appendCommandToData(variableName, "word", "0");
+        AssemblyFileWriter.appendCommandToCode("la", "$t1", variableName);
+        AssemblyFileWriter.appendCommandToCode("sw", "$t0", "0($t1)");
+        AssemblyFileWriter.appendDebugLine(variableName);
+        SemanticStack.push(new LocalVariableDescriptor(variableName, Type.INTEGER_NUMBER));
     }
 }
