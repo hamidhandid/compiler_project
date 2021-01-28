517e9837caf102bb4396cff1373fe705d0047fc7
Amin <yazdi.mohammad99@gmail.com>
2021-01-28 17:30:29 +0330

    Change parameters of And, Or constructors to Descriptor.

--- src/codegen/ast/expression/binary_expression/logical/logical_expressions/And.java
+++ src/codegen/ast/expression/binary_expression/logical/logical_expressions/And.java
@@ -2,10 +2,11 @@ package codegen.ast.expression.binary_expression.logical.logical_expressions;
 
 import codegen.ast.expression.Expression;
 import codegen.ast.expression.binary_expression.logical.LogicalExpression;
+import codegen.symbol_table.dscp.Descriptor;
 
 public class And extends LogicalExpression {
 
-    public And(Expression firstOperand, Expression secondOperand) {
+    public And(Descriptor firstOperand, Descriptor secondOperand) {
         super(firstOperand, secondOperand, "&");
     }
 
--- src/codegen/ast/expression/binary_expression/logical/logical_expressions/Or.java
+++ src/codegen/ast/expression/binary_expression/logical/logical_expressions/Or.java
@@ -2,10 +2,11 @@ package codegen.ast.expression.binary_expression.logical.logical_expressions;
 
 import codegen.ast.expression.Expression;
 import codegen.ast.expression.binary_expression.logical.LogicalExpression;
+import codegen.symbol_table.dscp.Descriptor;
 
 public class Or extends LogicalExpression {
 
-    public Or(Expression firstOperand, Expression secondOperand) {
+    public Or(Descriptor firstOperand, Descriptor secondOperand) {
         super(firstOperand, secondOperand, "||");
     }
 
