083e56a18b8040d7a649db8bea096f325239cdf9
Hamid <montazeria90@gmail.com>
2021-01-28 13:07:46 +0330

    Fix IntegerConstant code

--- src/codegen/ast/expression/constant/IntegerConstant.java
+++ src/codegen/ast/expression/constant/IntegerConstant.java
@@ -1,7 +1,13 @@
 package codegen.ast.expression.constant;
 
+import codegen.CodeGenerator;
+import codegen.symbol_table.SymbolTable;
+import codegen.symbol_table.dscp.Descriptor;
 import codegen.symbol_table.dscp.variables.LocalVariableDescriptor;
 import codegen.symbol_table.stacks.SemanticStack;
+import codegen.symbol_table.stacks.SymbolTableStack;
+import codegen.utils.AssemblyFileWriter;
+import scanner.classes.Type;
 
 public class IntegerConstant extends ConstantExpression{
     protected int intConst;
@@ -13,6 +19,14 @@ public class IntegerConstant extends ConstantExpression{
     @Override
     public void compile() {
         System.out.println(intConst);
-        SemanticStack.push(this);
+        String variableName = CodeGenerator.getVariableName();
+        Descriptor descriptor = new LocalVariableDescriptor(variableName, Type.INTEGER_NUMBER);
+        SemanticStack.push(descriptor);
+        AssemblyFileWriter.appendComment("integer constant");
+        AssemblyFileWriter.appendCommandToCode("li", "$t0", String.valueOf(intConst));
+        AssemblyFileWriter.appendCommandToCode("sw", "$t0", variableName);
+        SymbolTableStack.top().addDescriptor("$" + intConst, descriptor);
+        AssemblyFileWriter.appendCommandToData(variableName, "word", "0");
+        AssemblyFileWriter.appendDebugLine(variableName);
     }
 }
