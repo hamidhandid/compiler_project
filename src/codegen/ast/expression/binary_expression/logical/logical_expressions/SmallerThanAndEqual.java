5346cb777be99bce759df2bd1f545296eca14eaf
Amin <yazdi.mohammad99@gmail.com>
2021-01-28 18:07:40 +0330

    Change parameters of And, Or constructors to Descriptor.

--- src/codegen/ast/expression/binary_expression/logical/logical_expressions/SmallerThanAndEqual.java
+++ src/codegen/ast/expression/binary_expression/logical/logical_expressions/SmallerThanAndEqual.java
@@ -2,9 +2,11 @@ package codegen.ast.expression.binary_expression.logical.logical_expressions;
 
 import codegen.ast.expression.Expression;
 import codegen.ast.expression.binary_expression.logical.LogicalExpression;
+import codegen.symbol_table.dscp.Descriptor;
+
 public class SmallerThanAndEqual extends LogicalExpression {
 
-    public SmallerThanAndEqual(Expression firstOperand, Expression secondOperand) {
+    public SmallerThanAndEqual(Descriptor firstOperand, Descriptor secondOperand) {
         super(firstOperand, secondOperand, "<=");
     }
 
