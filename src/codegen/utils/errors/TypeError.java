d070e88f034e4209f1999f2bf80bc2b913bee285
Amin <yazdi.mohammad99@gmail.com>
2021-01-29 16:28:13 +0330

    Change parameters of TypeError to varArgs.

--- src/codegen/utils/errors/TypeError.java
+++ src/codegen/utils/errors/TypeError.java
@@ -2,7 +2,19 @@ package codegen.utils.errors;
 import scanner.classes.Type;
 
 public class TypeError extends RuntimeException {
-    public TypeError(String operation, Type firstType, Type secondType) {
-        super("TypeError: unsupported operand type(s) for" + operation + ": " + firstType + ", " + secondType);
+    private static String stringifyMultipleType(Type[] types) {
+        String returnVal = "";
+        for (Type s: types){
+            returnVal += s.toString() + ", ";
+        }
+        return returnVal;
     }
+
+    public TypeError(String operation, Type... Types) {
+        super("TypeError: unsupported operand type(s) for" + operation + ": " + stringifyMultipleType(Types));
+    }
+
+    /*public TypeError(String operation, Type firstType, Type secondType) {
+        super("TypeError: unsupported operand type(s) for" + operation + ": " + firstType + ", " + secondType);
+    }*/
 }
