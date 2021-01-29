a12ef13aea281f9cb8fd3d6a7be6951b5c873422
Hamid <montazeria90@gmail.com>
2021-01-29 16:35:55 +0330

    Fix solo if without else

--- src/codegen/ast/statement_block/statements/If.java
+++ src/codegen/ast/statement_block/statements/If.java
@@ -28,15 +28,19 @@ public class If extends Statement {
     public static void completeIf() {
         AssemblyFileWriter.appendComment("complete if code");
         AssemblyFileWriter.appendCommandToCode("j", afterElseLabel);
+        AssemblyFileWriter.addLabel(afterIfLabel);
+        AssemblyFileWriter.addLabel(afterElseLabel);
     }
 
     public static void elseCode() {
-        AssemblyFileWriter.addLabel(afterIfLabel);
         AssemblyFileWriter.appendComment("else code");
+        AssemblyFileWriter.deleteLabel(afterIfLabel);
+        AssemblyFileWriter.addLabel(afterIfLabel);
     }
 
     public static void completeElse() {
         AssemblyFileWriter.appendComment("complete else code");
+        AssemblyFileWriter.deleteLabel(afterElseLabel);
         AssemblyFileWriter.addLabel(afterElseLabel);
     }
 }
