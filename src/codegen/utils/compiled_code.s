2b99b0c928b0a9afdee8896aa40146dd3d4599b1
Hamid <montazeria90@gmail.com>
2021-01-30 15:40:40 +0330

    Fix parser

---
+++ src/codegen/utils/compiled_code.s
@@ -0,0 +1,91 @@
+.text
+.globl main
+main:
+		# integer constant 
+		li $t0, 3
+		sw $t0, adr2
+		# t7 is just for debugging 
+		lw $t7, adr2
+		# assignment adr1 = adr2 
+		la $t0, adr1
+		la $t1, adr2
+		lw $t1, 0($t1)
+		sw $t1, 0($t0)
+		# t7 is just for debugging 
+		lw $t7, adr1
+		# start condition of while 
+lbl1:
+		# integer constant 
+		li $t0, 6
+		sw $t0, adr3
+		# t7 is just for debugging 
+		lw $t7, adr3
+		# binary slt expression of adr1, adr3 
+		la $t0, adr1
+		la $t1, adr3
+		lw $t0, 0($t0)
+		lw $t1, 0($t1)
+		slt $t0, $t0, $t1
+		sw $t0, adr4
+		# t7 is just for debugging 
+		lw $t7, adr4
+		# while code for Descriptor{addressName='adr4', type=INTEGER_NUMBER, isLocal=true} 
+		la $t0, adr4
+		lw $t1, 0($t0)
+		beqz $t1, lbl2
+		# integer constant 
+		li $t0, 1
+		sw $t0, adr5
+		# t7 is just for debugging 
+		lw $t7, adr5
+		# binary add expression of adr1, adr5 
+		la $t0, adr1
+		la $t1, adr5
+		lw $t0, 0($t0)
+		lw $t1, 0($t1)
+		add $t0, $t0, $t1
+		sw $t0, adr6
+		# t7 is just for debugging 
+		lw $t7, adr6
+		# assignment adr1 = adr6 
+		la $t0, adr1
+		la $t1, adr6
+		lw $t1, 0($t1)
+		sw $t1, 0($t0)
+		# t7 is just for debugging 
+		lw $t7, adr1
+		# print integer (adr1) 
+		li $v0, 1
+		la $t0, adr1
+		lw $t0, 0($t0)
+		move $a0, $t0
+		syscall 
+		# new line 
+		li $v0, 4
+		la $a0, nl
+		syscall 
+		# end of while 
+		j lbl1
+lbl2:
+		# integer constant 
+		li $t0, 0
+		sw $t0, adr7
+		# t7 is just for debugging 
+		lw $t7, adr7
+		# return adr7 
+		li $v0, 10
+		la $t0, adr7
+		lw $t0, 0($t0)
+		move $a0, $t0
+		syscall 
+.data
+		nl: .asciiz "\n"
+		strbuffer: .space 20
+		stradr: .word 0
+		adr1: .word 0
+		adr2: .word 0
+		adr3: .word 0
+		adr4: .word 0
+		adr5: .word 0
+		adr6: .word 0
+		adr7: .word 0
